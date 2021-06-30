42f13b80d621ffbbc4df7328e5a1bfe6b272a6dd
Michael Woodyard <mbwoodyard@hotmail.com>
2021-06-30 12:35:01 -0400

    Added TODO's

--- public/script.js
+++ public/script.js
@@ -8,13 +8,19 @@
 // SECTION Variables                     //
 //———————————————————————————————————————//
 
-
+// TODO: In the future do a JSON file for the productList instead of this test variable.
+// NOTE: !!!TEST VARIABLE!!!
+    let productList = ["apple", "orange", "pear"];
+// NOTE: !!!TEST VARIABLE!!!
 
 //———————————————————————————————————————//
 // SECTION Functions                     //
 //———————————————————————————————————————//
 
-
+function searchbar() {
+    // TODO: Create proper amount of li's to generate the product list.
+    // TODO: In CSS, make the li's look sexy!
+}
 
 //———————————————————————————————————————//
 // SECTION Sockets                       //
