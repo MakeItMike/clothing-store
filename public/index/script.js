2c5729100a0ecdc811ff55c86c8a9a8b3f9ddf92
Michael Woodyard <mbwoodyard@hotmail.com>
2021-08-05 14:36:13 -0400

    Website Overhaul Removed ALL pages Simplified HTML, CSS Buttons

--- public/index/script.js
+++ public/index/script.js
@@ -1,3 +1,5 @@
+console.log("%cHello! Let me know how I can improve by dropping me an email at: %cmbwoodyard@hotmail.com", "font-size: 18px;", "font-size: 18px; color: lightblue; text-decoration: underline;");
+
 //———————————————————————————————————————//
 // SECTION TODO's                        //
 //———————————————————————————————————————//
@@ -10,28 +12,144 @@
 
 // TODO: In the future do a JSON file for the productList instead of this variable.
 // NOTE: !!!TEST VARIABLE!!!
-    let productList = {
-        product1: {
-            name: "Apple",
-            desc: "A delicious fruit."
-        },
+let productList = {
+    product1: {
+        name: "Apple",
+        desc: "A delicious fruit."
+    },
 
-        product2: {
-            name: "Orange",
-            desc: "A delicious fruit."
-        },
+    product2: {
+        name: "Orange",
+        desc: "A delicious fruit."
+    },
 
-        product2: {
-            name: "Pear",
-            desc: "A delicious fruit."
-        }
-    };
-    console.log("Product name: " + productList.product1.name + "\n" + "Product desc: " + productList.product1.desc);
-// NOTE: !!!TEST VARIABLE!!!
+    product2: {
+        name: "Pear",
+        desc: "A delicious fruit."
+    }
+};
+// console.log("Product name: " + productList.product1.name + "\n" + "Product desc: " + productList.product1.desc);
+
+let btnDebounce = false;
 
 //———————————————————————————————————————//
 // SECTION Functions                     //
 //———————————————————————————————————————//
+
+function buttons(page) {
+    if(!btnDebounce) {
+        let landing       = document.getElementById("landing-page")
+        let newArrivals   = document.getElementById("new-arrivals-page");
+        let men           = document.getElementById("men-page");
+        let women         = document.getElementById("women-page");
+        let boys          = document.getElementById("boys-page");
+        let girls         = document.getElementById("girls-page");
+        let genderNeutral = document.getElementById("gender-neutral-page");
+		let signUp		  = document.getElementById("sign-up-page");
+		let logIn		  = document.getElementById("log-in-page");
+
+        switch(page) {
+            case 0: // Landing
+				landing.classList.remove("hide");
+				newArrivals.classList.add("hide");
+				men.classList.add("hide");
+				women.classList.add("hide");
+				boys.classList.add("hide");
+				girls.classList.add("hide");
+				genderNeutral.classList.add("hide");
+            break;
+
+            case 1: // New Arrivals
+				landing.classList.add("hide");
+				newArrivals.classList.remove("hide");
+				men.classList.add("hide");
+				women.classList.add("hide");
+				boys.classList.add("hide");
+				girls.classList.add("hide");
+				genderNeutral.classList.add("hide");
+            break;
+
+            case 2: // Men
+				landing.classList.add("hide");
+				newArrivals.classList.add("hide");
+				men.classList.remove("hide");
+				women.classList.add("hide");
+				boys.classList.add("hide");
+				girls.classList.add("hide");
+				genderNeutral.classList.add("hide");
+            break;
+
+            case 3: // Women
+				landing.classList.add("hide");
+				newArrivals.classList.add("hide");
+				men.classList.add("hide");
+				women.classList.remove("hide");
+				boys.classList.add("hide");
+				girls.classList.add("hide");
+				genderNeutral.classList.add("hide");
+            break;
+
+            case 4: // Boys
+				landing.classList.add("hide");
+				newArrivals.classList.add("hide");
+				men.classList.add("hide");
+				women.classList.add("hide");
+				boys.classList.remove("hide");
+				girls.classList.add("hide");
+				genderNeutral.classList.add("hide");
+            break;
+
+            case 5: // Girls
+				landing.classList.add("hide");
+				newArrivals.classList.add("hide");
+				men.classList.add("hide");
+				women.classList.add("hide");
+				boys.classList.add("hide");
+				girls.classList.remove("hide");
+				genderNeutral.classList.add("hide");
+            break;
+
+            case 6: // Gender Neutral
+				landing.classList.add("hide");
+				newArrivals.classList.add("hide");
+				men.classList.add("hide");
+				women.classList.add("hide");
+				boys.classList.add("hide");
+				girls.classList.add("hide");
+				genderNeutral.classList.remove("hide");
+            break;
+
+            case 7: // Sign Up
+				landing.classList.add("hide");
+				newArrivals.classList.add("hide");
+				men.classList.add("hide");
+				women.classList.add("hide");
+				boys.classList.add("hide");
+				girls.classList.add("hide");
+				genderNeutral.classList.add("hide");
+				signUp.classList.remove("hide");
+				logIn.classList.add("hide");
+            break;
+
+            case 8: // Log In
+				landing.classList.add("hide");
+				newArrivals.classList.add("hide");
+				men.classList.add("hide");
+				women.classList.add("hide");
+				boys.classList.add("hide");
+				girls.classList.add("hide");
+				genderNeutral.classList.add("hide");
+				signUp.classList.add("hide");
+				logIn.classList.remove("hide");
+            break;
+
+        }
+
+        setTimeout(() => {
+			btnDebounce = false;
+		}, 250); // waits 250ms (0.5s)
+    }
+}
 
 function searchBar(key) {
     // TODO: Create proper amount of li's to generate product list.
